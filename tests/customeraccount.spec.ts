eee28f0e97e28fb88b5171eb777cd112ba7a8579
JoshVillagomez <eljelix99@gmail.com>
2024-05-21 01:06:26 -0600

    added customeraccount objects to homepage pageobect

--- tests/customeraccount.spec.ts
+++ tests/customeraccount.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from './pageobjects/homepage';
 
-test('login to liverpool using credentials', async ({ page }) => {
+test('login to liverpool using correct credentials', async ({ page }) => {
   await page.goto('https://www.liverpool.com.mx/tienda/home');
 
   const login = new LoginPage(page)
@@ -15,27 +15,30 @@ test('login to liverpool using credentials', async ({ page }) => {
   await page.getByRole('heading', { name: 'Pantalla Smart TV Hisense LED de 50 pulgadas 4K/UHD 50A7K con Google TV' }).isVisible();
 */
 
-
-  // Expects page to have a heading with the name of Installation.
-  //await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
-test('Find the article i was looking for', async ({ page }) => {
-    await page.goto('https://www.liverpool.com.mx/tienda/home');
-  
-    // Click the get started link.
-    await page.getByRole('textbox', { name: 'Buscar por producto, marca y más...' }).isVisible();
-    await page.getByRole('textbox', { name: 'Buscar por producto, marca y más...' }).fill('Pantalla Smart TV Samsung LED de 55 pulgadas HD')
-    await page.keyboard.press('Enter')
-    await page.getByRole('heading', { name: 'Pantalla Smart TV Samsung LED de 55 pulgadas HD Un55cu8000fxzx con Tizen' }).isVisible();
-    await page.getByRole('heading', { name: 'Pantalla Smart TV Samsung LED de 55 pulgadas HD Un55cu8000fxzx con Tizen' }).click();
-    await page.getByRole('button', { name: 'Comprar ahora' }).isVisible();
-    await page.getByRole('button', { name: 'Comprar ahora' }).click();
-    await page.getByRole('button', { name: 'Inicia sesión' }).isVisible();
- 
-  });
+test ('Verify the account creation form', async ({ page }) => {
+  await page.goto('https://www.liverpool.com.mx/tienda/home');
 
-  test('Creating a customer account', async ({ page }) => {
+  const login = new LoginPage(page)
+
+  await login.loginToCreateAccount('eljelix99+team2@gmail.com', '59xTphCiJKZ')
+});
+
+test ('Create a new consumer account with valid data', async ({ page }) => {
+
+
+});
+
+test ('Create a new consumer account with invalid email format', async ({ page }) => {
+  await page.goto('https://www.liverpool.com.mx/tienda/home');
+
+  const login = new LoginPage(page)
+
+  await login.loginToCreateAccountEmailNotValid('eljelix99+team2.gmail.com', '59xTphCiJKZ')
+  
+});
+  /*test('Creating a customer account', async ({ page }) => {
     await page.goto('https://www.liverpool.com.mx/tienda/home');
   
     // Click the get started link.
@@ -57,6 +60,6 @@ test('Find the article i was looking for', async ({ page }) => {
     
     //await page.getByRole('button', { name: 'Comprar ahora' }).click();
  
-  });
+  });*/
 
   
--- tests/pageobjects/homepage.ts
+++ tests/pageobjects/homepage.ts
@@ -6,12 +6,20 @@ export class LoginPage{
     private readonly usernameTextbox: Locator
     private readonly passwordTextbox: Locator
     private readonly loginButton: Locator
+    private readonly createAccountLink: Locator
+    private readonly createAccountButton: Locator
+    private readonly createAccountHeaderDisplayed: Locator
+    private readonly emailNotValidMessage: Locator
 
     constructor(page : Page){
         this.loginPageLink = page.locator('//span[@class="a-header__topLink"]')
         this.usernameTextbox = page.getByRole('textbox', { name: 'Correo electrónico*' })
         this.passwordTextbox = page.getByRole('textbox', { name: 'Contraseña*' })
         this.loginButton = page.getByRole('button', { name: 'Iniciar sesión' })
+        this.createAccountLink = page.getByRole('link', { name: 'Crear cuenta' })
+        this.createAccountButton = page.getByRole('button', { name: 'Crear cuenta' })
+        this.createAccountHeaderDisplayed = page.locator('//div[@class="m-title"]')
+        this.emailNotValidMessage = page.locator('//span[@id="error-element-email"]')
     }
 
     async clickOnLoginPageLink(){
@@ -28,10 +36,43 @@ export class LoginPage{
         await this.loginButton.click()
     }
 
+    async clickOnCreateAccountLink() {
+        await this.createAccountLink.click();
+    }
+
+    async clickOnCreateAccountButton() {
+        await this.createAccountButton.click();
+    }
+
+    async createAccountHeader() {
+        await this.createAccountHeaderDisplayed.isVisible()
+    }
+
+    async emailNotValid(){
+        await this.emailNotValidMessage.isVisible()
+    }
+
     async loginWithCredentials(username:string, password:string){
         await this.clickOnLoginPageLink()
         await this.fillUsername(username)
         await this.fillPassword(password)
         await this.clickOnLogin()
+    }
+
+    async loginToCreateAccount(username:string, password:string){
+        await this.clickOnLoginPageLink()
+        await this.clickOnCreateAccountLink()
+        await this.fillUsername(username)
+        await this.fillPassword(password)
+        await this.clickOnCreateAccountButton()
+        await this.createAccountHeader()
+    }
+    async loginToCreateAccountEmailNotValid(username:string, password:string){
+        await this.clickOnLoginPageLink()
+        await this.clickOnCreateAccountLink()
+        await this.fillUsername(username)
+        await this.fillPassword(password)
+        await this.clickOnCreateAccountButton()
+        await this.emailNotValid()
     }
 }
