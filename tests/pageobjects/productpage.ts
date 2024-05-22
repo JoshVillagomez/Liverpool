fe06bdb99893e3c2d1f8a390156206f7da06113e
JoshVillagomez <eljelix99@gmail.com>
2024-05-22 10:22:23 -0600

    add mailslurp plugin

--- tests/pageobjects/productpage.ts
+++ tests/pageobjects/productpage.ts
@@ -1,1 +1,47 @@
 import { Locator, Page } from "@playwright/test";
+
+export class ProductPage{
+    private readonly searchBar: Locator
+    private readonly buyNowButton: Locator
+    private readonly productTVHeading: Locator
+
+
+constructor(page: Page){
+    this.searchBar = page.getByRole('textbox', { name: 'Buscar por producto, marca y más...' })
+    this.productTVHeading = page.getByRole('heading', { name: 'Pantalla Smart TV Samsung LED de 55 pulgadas HD Un55cu8000fxzx con Tizen' })
+    this.buyNowButton = page.getByRole('button', { name: 'Comprar ahora' })
+
+}
+
+async searchBarVisible(){
+    await this.searchBar.isVisible()
+}
+
+async searchBarFill(product:string){
+    await this.searchBar.fill(product)
+}
+
+async buyNowButtonVisible(){
+    await this.buyNowButton.isVisible()
+}
+async buyNowButtonClick(){
+    await this.buyNowButton.click()
+}
+async productTVClick(){
+    await this.productTVHeading.click()
+}
+
+async searchForAnSpecificProduct(product:string){
+    await this.searchBarVisible()
+    await this.searchBarFill(product)
+    
+ 
+}
+async buyNowButtonFunctionality(){
+    await this.productTVClick()
+    await this.buyNowButtonVisible()
+    await this.buyNowButtonClick()
+}
+
+
+}
--- tests/searchbar.spec.ts
+++ tests/searchbar.spec.ts
@@ -1,10 +1,18 @@
 import { test, expect } from '@playwright/test';
+import { ProductPage } from './pageobjects/productpage';
 
 
 test('Find the article i was looking for', async ({ page }) => {
     await page.goto('https://www.liverpool.com.mx/tienda/home');
+
+    const login = new ProductPage(page)
+
+  await login.searchForAnSpecificProduct('Pantalla Smart TV Samsung LED de 55 pulgadas HD Un55cu8000fxzx con Tizen')
+  await page.keyboard.press('Enter')
+  await login.buyNowButtonFunctionality()
+  await page.pause()
   
-    // Click the get started link.
+    /* Click the get started link.
     await page.getByRole('textbox', { name: 'Buscar por producto, marca y más...' }).isVisible();
     await page.getByRole('textbox', { name: 'Buscar por producto, marca y más...' }).fill('Pantalla Smart TV Samsung LED de 55 pulgadas HD')
     await page.keyboard.press('Enter')
@@ -13,5 +21,9 @@ test('Find the article i was looking for', async ({ page }) => {
     await page.getByRole('button', { name: 'Comprar ahora' }).isVisible();
     await page.getByRole('button', { name: 'Comprar ahora' }).click();
     await page.getByRole('button', { name: 'Inicia sesión' }).isVisible();
- 
+ */
   });
+
+  test('Search for a product that is not available/nonexistent', async ({ page }) =>{
+
+  })
